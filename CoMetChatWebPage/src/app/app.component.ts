55244872266b19acf3d282676ca33b99c9a41873
Akshay Pise <64837471+akshay-pise@users.noreply.github.com>
2023-03-24 00:57:59 +0530

    injectable file added

--- CoMetChatWebPage/src/app/app.component.ts
+++ CoMetChatWebPage/src/app/app.component.ts
@@ -4,19 +4,29 @@ import { ServiceService } from './core/service/service.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ServiceService]
 })
 export class AppComponent {
 
-  puserChatList:any[]=[]
-  constructor(private serv: ServiceService) { }
+  puserChatList:any[]=[];
+  constructor(private serv: ServiceService) {
+    this.puserChatList = serv.getCustomers();
+    console.log(this.puserChatList);
+  }
 
   ngOnInit(): void {
-    this.getUserChatList();
+    // this.getUserChatList();
   }
-  getUserChatList() {
-    this.serv.getUserList().subscribe((result: any) => {
-      this.puserChatList = result.results;
-    })
-  }
+  // getUserChatList() {
+    // this.serv.getUserList().subscribe((result: any) => {
+    //   this.puserChatList = result.results;
+    // })
+    // this.puserChatList=this.serv
+  // }
+  // customers: Customer[];
+
+  // constructor(service: Service) {
+  //   this.customers = service.getCustomers();
+  // }
 }
